913bb7f7d4a65e648dc8cf2f9211c58e7c67a1c6
stas-notebook <matveev.stanislav@yahoo.com>
2022-07-26 21:02:10 +0300

    add time to posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from "next";
-import { collection } from "firebase/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { db } from "../../app/firebaseApp";
 import Link from "next/link";
 import postConverter from "../../helpers/postConverter";
 
 const Posts: NextPage = () => {
+  const postsRef = collection(db, "posts").withConverter(postConverter);
   const [posts] = useCollectionData(
-    collection(db, "posts").withConverter(postConverter)
+    query(postsRef, orderBy("createdAt", "desc"))
   );
   return (
     <div>
@@ -17,7 +18,7 @@ const Posts: NextPage = () => {
           <div key={post.id}>
             {post.createdAt && (
               <span>{post.createdAt.toLocaleDateString()}</span>
-            )}
+            )}{" "}
             <Link href={`/posts/${post.id}`}>{post.text}</Link>
           </div>
         ))}
